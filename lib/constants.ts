c34ccf3171b45ea5011858057596c185b2393ab5
akdattahistorical108-png <akdatta.historical.108@gmail.com>
2026-03-24 21:39:41 +0600

    Add MANDAL_INFO data and COLORS configuration in lib/constants.ts

---
+++ lib/constants.ts
@@ -0,0 +1,20 @@
+export const MANDAL_INFO = [
+  { mandal: 1, bengali: 'মণ্ডল ১', suktas: 191 },
+  { mandal: 2, bengali: 'মণ্ডল ২', suktas: 43 },
+  { mandal: 3, bengali: 'মণ্ডল ৩', suktas: 62 },
+  { mandal: 4, bengali: 'মণ্ডল ৪', suktas: 58 },
+  { mandal: 5, bengali: 'মণ্ডল ৫', suktas: 87 },
+  { mandal: 6, bengali: 'মণ্ডল ৬', suktas: 75 },
+  { mandal: 7, bengali: 'মণ্ডল ৭', suktas: 104 },
+  { mandal: 8, bengali: 'মণ্ডল ৮', suktas: 103 },
+  { mandal: 9, bengali: 'মণ্ডল ৯', suktas: 114 },
+  { mandal: 10, bengali: 'মণ্ডল ১০', suktas: 191 },
+]
+
+export const COLORS = {
+  primary: '#FF6B00',
+  dark: '#8B0000',
+  cream: '#FFF8E7',
+  gold: '#D4AF37',
+  brown: '#3E1C00',
+}
